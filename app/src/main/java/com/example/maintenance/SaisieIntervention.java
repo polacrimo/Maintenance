0ce50a11e731b380cede3d3189c198b7c7dbe1ab
Florent DAGNOT <florent.dagnot@laposte.net>
2022-06-22 14:12:28 +0200

    Creation activities Saisie

---
+++ app/src/main/java/com/example/maintenance/SaisieIntervention.java
@@ -0,0 +1,14 @@
+package com.example.maintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaisieIntervention extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisie_intervention);
+    }
+}
---
+++ app/src/main/java/com/example/maintenance/SaisieProprietaire.java
@@ -0,0 +1,14 @@
+package com.example.maintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaisieProprietaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisie_proprietaire);
+    }
+}
