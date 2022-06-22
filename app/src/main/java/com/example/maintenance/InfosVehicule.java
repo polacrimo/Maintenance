6c4009c10538d6427bca338c09c37b4d5c3e07af
Florent DAGNOT <florent.dagnot@laposte.net>
2022-06-22 15:39:15 +0200

    Infos Activities init

---
+++ app/src/main/java/com/example/maintenance/InfosVehicule.java
@@ -0,0 +1,14 @@
+package com.example.maintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfosVehicule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infos_vehicule);
+    }
+}
