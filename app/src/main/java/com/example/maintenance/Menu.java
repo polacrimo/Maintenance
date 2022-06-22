e0452cdd343af9e82f7f7a7a84e029bb9cdb8849
Florent DAGNOT <florent.dagnot@laposte.net>
2022-06-22 14:43:41 +0200

    Menu Code + Listes Activities init

--- app/src/main/java/com/example/maintenance/Menu.java
+++ app/src/main/java/com/example/maintenance/Menu.java
@@ -2,13 +2,87 @@ package com.example.maintenance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
+    Button retourBtn;
+    Button sVehiculeBtn;
+    Button sInterventionBtn;
+    Button sProtrietaireBtn;
+    Button lVehiculeBtn;
+    Button lInterventionBtn;
+    Button lProtrietaireBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        retourBtn = (Button) findViewById(R.id.retour);
+        sVehiculeBtn = (Button) findViewById(R.id.saisieV);
+        sInterventionBtn = (Button) findViewById(R.id.saisieI);
+        sProtrietaireBtn = (Button) findViewById(R.id.saisieP);
+        lVehiculeBtn = (Button) findViewById(R.id.listeV);
+        lInterventionBtn = (Button) findViewById(R.id.listeI);
+        lProtrietaireBtn = (Button) findViewById(R.id.listeP);
+
+        retourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        sVehiculeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, SaisieVehicule.class);
+                startActivity(intent);
+            }
+        });
+
+        sInterventionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, SaisieIntervention.class);
+                startActivity(intent);
+            }
+        });
+
+        sProtrietaireBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, SaisieProprietaire.class);
+                startActivity(intent);
+            }
+        });
+
+        lVehiculeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, ListeVehicule.class);
+                startActivity(intent);
+            }
+        });
+
+        lInterventionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, ListeIntervention.class);
+                startActivity(intent);
+            }
+        });
+
+        lProtrietaireBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, ListeProprietaire.class);
+                startActivity(intent);
+            }
+        });
     }
 }
