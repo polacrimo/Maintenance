80ddba243d97ffcf1af1664d0b7f4e28af0b5cf4
Alex CZECH <polskalex@gmail.com>
2022-06-23 14:36:56 +0200

    Avancée sur la saisie véhicule

--- app/src/main/java/com/example/maintenance/SaisieVehicule.java
+++ app/src/main/java/com/example/maintenance/SaisieVehicule.java
@@ -3,12 +3,19 @@ package com.example.maintenance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SaisieVehicule extends AppCompatActivity {
+    TextView view1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        view1 = (TextView) findViewById(R.id.view1);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisie_vehicule);
+
+        view1.setText("Hello");
     }
 }
